14e53f5d6140f0edb294e68408cc703a1aa7d13f
lslslscai <2052366734@qq.com>
2020-04-21 21:12:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Welcome to My WebSite!<br/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -19,7 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <p>Welcome to My WebSite!</p>
+      
     </div>
   );
 }
